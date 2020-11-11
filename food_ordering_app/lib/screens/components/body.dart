0ec7c45acfcb5a695e26d300433fc8ce013aa53f
n0y0j <n0y0j77@gmail.com>
2020-11-11 22:24:04 +0900

    feat: Implement ItemList.

--- food_ordering_app/lib/screens/components/body.dart
+++ food_ordering_app/lib/screens/components/body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_ordering_app/components/search_box.dart';
+import 'package:food_ordering_app/constants.dart';
 import 'package:food_ordering_app/screens/components/category_list.dart';
+import 'package:food_ordering_app/screens/components/item_list.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -10,7 +13,8 @@ class Body extends StatelessWidget {
         SearchBox(
           onChanged: (value) {},
         ),
-        CategoryList()
+        CategoryList(),
+        ItemList()
       ],
     );
   }
---
+++ food_ordering_app/lib/screens/components/item_list.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:food_ordering_app/screens/components/item_card.dart';
+
+class ItemList extends StatelessWidget {
+  const ItemList({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: <Widget>[
+          ItemCard(
+            svgSrc: "assets/icons/burger_beer.svg",
+            title: "Burger & Beer",
+            shopName: "MacDonald`s",
+            press: () {},
+          ),
+          ItemCard(
+            svgSrc: "assets/icons/chinese_noodles.svg",
+            title: "Chinese & Noodles",
+            shopName: "Wendys",
+            press: () {},
+          ),
+          ItemCard(
+            svgSrc: "assets/icons/burger_beer.svg",
+            title: "Burger & Beer",
+            shopName: "MacDonald`s",
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
