6767e9f2f69707990d77f8a2918587e525cb6551
n0y0j <n0y0j77@gmail.com>
2020-11-11 22:40:42 +0900

    feat: Implement DiscountCard.

--- food_ordering_app/lib/screens/components/body.dart
+++ food_ordering_app/lib/screens/components/body.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_ordering_app/components/search_box.dart';
-import 'package:food_ordering_app/constants.dart';
 import 'package:food_ordering_app/screens/components/category_list.dart';
+import 'package:food_ordering_app/screens/components/discount_card.dart';
 import 'package:food_ordering_app/screens/components/item_list.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         SearchBox(
           onChanged: (value) {},
         ),
         CategoryList(),
-        ItemList()
+        ItemList(),
+        DiscountCard()
       ],
     );
   }
