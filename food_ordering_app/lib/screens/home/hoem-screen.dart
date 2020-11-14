4feb425fa52f8b7ebb4edc62f186487f6860327d
n0y0j <n0y0j77@gmail.com>
2020-11-14 16:32:08 +0900

    feat: Implement bottom navigation view.

--- food_ordering_app/lib/screens/home/hoem-screen.dart
+++ food_ordering_app/lib/screens/home/hoem-screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:food_ordering_app/components/bottom_nav_bar.dart';
 import 'package:food_ordering_app/screens/components/app_bar.dart';
 import 'package:food_ordering_app/screens/components/body.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.top]);
     return Scaffold(
       appBar: homeAppbar(context),
+      bottomNavigationBar: BottomNavBar(),
       body: Body(),
     );
   }
