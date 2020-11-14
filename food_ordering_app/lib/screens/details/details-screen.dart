f695393637a32035b2cdffc03636bdc792c1e02b
n0y0j <n0y0j77@gmail.com>
2020-11-14 16:54:57 +0900

    feat: Implement details page body.

--- food_ordering_app/lib/screens/details/details-screen.dart
+++ food_ordering_app/lib/screens/details/details-screen.dart
@@ -8,6 +8,50 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: kPrimaryColor,
       appBar: detailsAppBar(),
+      body: Body(),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemImage(
+          imaSrc: "assets/images/burger.png",
+        ),
+        Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ItemImage extends StatelessWidget {
+  final String imaSrc;
+  const ItemImage({
+    Key key,
+    this.imaSrc,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Image.asset(
+      imaSrc,
+      height: size.height * 0.25,
+      // it cover the 25% of total height
+      width: double.infinity,
+      fit: BoxFit.fill,
     );
   }
 }
