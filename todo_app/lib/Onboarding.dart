3bc92fcb3fb2794644a996edd6e427711552aca6
n0y0j <n0y0j77@gmail.com>
2020-12-06 21:42:15 +0900

    feat: Implement intro page.

---
+++ todo_app/lib/Onboarding.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: onboarding(),
+    );
+  }
+}
+
+class onboarding extends StatefulWidget {
+  @override
+  _onboardingState createState() => _onboardingState();
+}
+
+class _onboardingState extends State<onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:todo_app/Onboarding.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,7 +27,28 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
+  void initState() {
+    Timer(Duration(seconds: 3), openOnBoard);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('asset/image/aking.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void openOnBoard() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => Onboarding()));
   }
 }
