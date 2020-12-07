53cb3ba3ef29bf7876b8e3db3179748d3ce1aab1
n0y0j <n0y0j77@gmail.com>
2020-12-07 20:53:23 +0900

    feat: Implement password reset success page.

--- todo_app/lib/SuccessPage.dart
+++ todo_app/lib/SuccessPage.dart
@@ -1,8 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/HomePage.dart';
 
 class SuccessPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'avenir',
+      ),
+      home: successPage(),
+    );
+  }
+}
+
+class successPage extends StatefulWidget {
+  @override
+  _successPageState createState() => _successPageState();
+}
+
+class _successPageState extends State<successPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("asset/image/success.png"),
+              ),
+            ),
+          ),
+          Text(
+            "Successful!",
+            style: TextStyle(
+              fontSize: 35,
+            ),
+          ),
+          Text(
+            "You have successfully changed our password. Please use your new password to login!",
+            style: TextStyle(
+              fontSize: 18,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 70),
+          Center(
+            child: InkWell(
+              onTap: openHomePage,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 100, vertical: 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(7)),
+                  color: Color(0xfff96060),
+                ),
+                child: Text(
+                  "Continue",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void openHomePage() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomePage()));
   }
 }
