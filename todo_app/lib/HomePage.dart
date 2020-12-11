5031be8041e8eb217edba146de07ca4be162d507
n0y0j <n0y0j77@gmail.com>
2020-12-11 20:12:43 +0900

    feat: Implement home page task widget.

--- todo_app/lib/HomePage.dart
+++ todo_app/lib/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -133,26 +134,34 @@ class _homePageState extends State<homePage> {
                     )
                   : Container(),
               Expanded(
-                  child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(height: 10),
-                    Container(
-                      padding: EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Today ${monthNames[today.month - 1]}, ${today.day}/${today.year}",
-                            style: TextStyle(fontSize: 18, color: Colors.grey),
-                          )
-                        ],
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 10),
+                      Container(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Today ${monthNames[today.month - 1]}, ${today.day}/${today.year}",
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.grey),
+                            )
+                          ],
+                        ),
                       ),
-                    )
-                  ],
+                      taskWidget(
+                          Color(0xfff96060), "Meeting with someone", "9:00 AM"),
+                      taskWidget(
+                          Colors.blue, "Meeting with someone", "9:00 AM"),
+                      taskWidget(
+                          Colors.green, "Take your medicines", "9:00 AM"),
+                    ],
+                  ),
                 ),
-              ))
+              )
             ],
           ),
         ],
@@ -164,5 +173,81 @@ class _homePageState extends State<homePage> {
     setState(() {
       filterType = filter;
     });
+  }
+
+  Slidable taskWidget(Color color, String title, String time) {
+    return Slidable(
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.3,
+      child: Container(
+        height: 80,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.03),
+              offset: Offset(0, 9),
+              blurRadius: 20,
+              spreadRadius: 1,
+            )
+          ],
+        ),
+        child: Row(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              height: 25,
+              width: 25,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                border: Border.all(color: color, width: 4),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                  ),
+                ),
+                Text(
+                  time,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+            Expanded(child: Container()),
+            Container(
+              height: 50,
+              width: 5,
+              color: color,
+            )
+          ],
+        ),
+      ),
+      secondaryActions: [
+        IconSlideAction(
+          caption: "Edit",
+          color: Colors.white,
+          icon: Icons.edit,
+          onTap: () {},
+        ),
+        IconSlideAction(
+          caption: "Delete",
+          color: color,
+          icon: Icons.edit,
+          onTap: () {},
+        ),
+      ],
+    );
   }
 }
