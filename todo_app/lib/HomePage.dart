9ccd0f1be867d3c5d03ff97e031d7fd98bc9a2f9
n0y0j <n0y0j77@gmail.com>
2020-12-11 19:24:25 +0900

    feat: Implement home page app bar.

--- todo_app/lib/HomePage.dart
+++ todo_app/lib/HomePage.dart
@@ -3,6 +3,55 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'avenir',
+      ),
+      home: homePage(),
+    );
+  }
+}
+
+class homePage extends StatefulWidget {
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AppBar(
+                centerTitle: true,
+                backgroundColor: Color(0xfff96060),
+                elevation: 0,
+                title: Text(
+                  "Work List",
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                actions: [
+                  IconButton(
+                    icon: Icon(
+                      Icons.short_text,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
